2535638e65b02ee3b4041865a134b0b9686a1e5a
kopedacpsc <kopedacpsc@gmail.com>
2016-03-21 14:17:43 -0700

    final draft try

--- CardFlipGame.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-    $('.flip').click(function(){
-        $(this).find('.card').addClass('flipped').mouseleave(function(){
-            $(this).removeClass('flipped');
-        });
-        return false;
-    });
